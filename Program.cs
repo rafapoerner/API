99c4a32502bd1865dfcc07d57f82684b062b9272
Rafa Poerner <rafa_poerner@hotmail.com>
2022-05-27 21:28:56 -0300

    Ajustes finais

--- Program.cs
+++ Program.cs
@@ -68,10 +68,11 @@ app.MapPut("/products/{id}", ([FromRoute] int id, ProductRequest productRequest,
     return Results.Ok();
 });
 
-app.MapDelete("/products/{code}", ([FromRoute] string code) =>
+app.MapDelete("/products/{id}", ([FromRoute] int id, ApplicationDbContext context) =>
 {
-    var productSaved = ProductRepository.GetBy(code);
-    ProductRepository.Remove(productSaved);
+    var product = context.Products.Where(p => p.Id == id).First();
+    context.Products.Remove(product);
+    context.SaveChanges();
     return Results.Ok();
 });
 
